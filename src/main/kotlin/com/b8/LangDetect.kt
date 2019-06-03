f28425127a0619c0afa4af7d5453beffb1e2c959
naor <naor2205@gmail.com>
2019-06-03 12:30:18 +0300

    add mapping from language code to full name

--- src/main/kotlin/com/b8/LangDetect.kt
+++ src/main/kotlin/com/b8/LangDetect.kt
@@ -11,13 +11,131 @@ const val MIN_NUM_OF_BYTES = 0
 const val MAX_NUM_OF_BYTES = 10000
 const val SINGLE_LANGUAGE_DETECTION_BUFFER_SIZE = 100
 const val MULTI_LANGUAGE_DETECTION_BUFFER_SIZE = 300
+const val UNKNOWN_LANGUAGE = "UNKNOWN"
 
 val LIB_PATH = "${System.getProperty("user.dir")}/src/main/lib/"
 
 
-data class LangDetectResponse(val probability: Float, val proportion: Float, val isReliable: Boolean, val language: String)
+data class LangDetectResponse(
+    val probability: Float,
+    val proportion: Float,
+    val isReliable: Boolean,
+    val language: String
+)
 
 class LangDetect : AutoCloseable {
+
+    private val fullLanguageName = mapOf(
+        "eo" to "Esperanto",
+        "co" to "Corsican",
+        "eu" to "Basque",
+        "ta" to "Tamil",
+        "de" to "German",
+        "mt" to "Maltese",
+        "ps" to "Pushto",
+        "te" to "Telugu",
+        "su" to "Sundanese",
+        "uz" to "Uzbek",
+        "ne" to "Nepali",
+        "nl" to "Dutch",
+        "sw" to "Swahili",
+        "sq" to "Albanian",
+        "ja" to "Japanese",
+        "no" to "Norwegian",
+        "mn" to "Mongolian",
+        "so" to "Somali",
+        "ko" to "Korean",
+        "kk" to "Kazakh",
+        "sl" to "Slovenian",
+        "mr" to "Marathi",
+        "th" to "Thai",
+        "zu" to "Zulu",
+        "ml" to "Malayalam",
+        "hr" to "Croatian",
+        "bs" to "Bosnian",
+        "lo" to "Lao",
+        "sd" to "Sindhi",
+        "cy" to "Welsh",
+        "hy" to "Armenian",
+        "uk" to "Ukrainian",
+        "pt" to "Portuguese",
+        "lv" to "Latvian",
+        "cs" to "Czech",
+        "vi" to "Vietnamese",
+        "jv" to "Javanese",
+        "be" to "Belarusian",
+        "km" to "Khmer",
+        "mk" to "Macedonian",
+        "tr" to "Turkish",
+        "fy" to "Western Frisian",
+        "am" to "Amharic",
+        "zh" to "Chinese",
+        "da" to "Danish",
+        "sv" to "Swedish",
+        "fi" to "Finnish",
+        "ht" to "Haitian; Haitian Creole",
+        "af" to "Afrikaans",
+        "la" to "Latin",
+        "id" to "Indonesian",
+        "sm" to "Samoan",
+        "ca" to "Catalan",
+        "ka" to "Georgian",
+        "sr" to "Serbian",
+        "it" to "Italian",
+        "sk" to "Slovak",
+        "ru" to "Russian",
+        "bg" to "Bulgarian",
+        "ny" to "Nyanja; Chichewa; Chewa",
+        "fa" to "Persian",
+        "gl" to "Galician",
+        "et" to "Estonian",
+        "ms" to "Malay",
+        "gd" to "Gaelic; Scottish Gaelic",
+        "ha" to "Hausa",
+        "is" to "Icelandic",
+        "ur" to "Urdu",
+        "mi" to "Maori",
+        "hi" to "Hindi",
+        "bn" to "Bengali",
+        "fr" to "French",
+        "yi" to "Yiddish",
+        "hu" to "Hungarian",
+        "xh" to "Xhosa",
+        "my" to "Burmese",
+        "tg" to "Tajik",
+        "ro" to "Romanian",
+        "ar" to "Arabic",
+        "lb" to "Luxembourgish",
+        "kn" to "Kannada",
+        "az" to "Azerbaijani",
+        "si" to "Sinhala; Sinhalese",
+        "ky" to "Kirghiz",
+        "mg" to "Malagasy",
+        "en" to "English",
+        "gu" to "Gujarati",
+        "es" to "Spanish",
+        "pl" to "Polish",
+        "ga" to "Irish",
+        "lt" to "Lithuanian",
+        "sn" to "Shona",
+        "yo" to "Yoruba",
+        "pa" to "Panjabi",
+        "ku" to "Kurdish",
+        "zh-Latn" to "Chinese-Latin",
+        "bg-Latn" to "Bulgarian-Latin",
+        "ru-Latn" to "Russian-Latin",
+        "hi-Latn" to "Hindi-Latin",
+        "ja-Latn" to "Japanese-Latin",
+        "st" to "Sotho,Southern",
+        "el" to "Greek",
+        "el-Latn" to "Greek-Latin",
+        "hmn" to "Hmong, Mong",
+        "ig" to "Igbo",
+        "iw" to "Hebrew",
+        "fil" to "Filipino",
+        "haw" to "Hawaiian",
+        "ceb" to "Cebuano"
+    )
 
     private var ptr: Long = -1
     private val detector: NativeLangDetector
@@ -62,6 +180,9 @@ class LangDetect : AutoCloseable {
 
     }
 
+    fun changeLanguageCodeToFullName(languageCode: String): String {
+        return fullLanguageName[languageCode] ?: UNKNOWN_LANGUAGE
+    }
 
     override fun close() {
         detector.deallocate(ptr)
